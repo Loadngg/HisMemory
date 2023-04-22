16d1fb37aadc25ef04224c0f9ab9c5bba3d854a2
Ксения Незнанова <kseniya052004@gmail.com>
2023-04-22 21:11:13 +0300

    > some changes

--- assets/js/indexPage.js
+++ assets/js/indexPage.js
@@ -1,9 +1,21 @@
 // Form logic
-const form_block = document.getElementById("form")
-const form_title_button = document.getElementById("form-title-button")
-form_title_button.onclick = () => form_block.classList.toggle("hidden")
-const form_button = document.getElementById("form-send-button")
-form_button.onclick = () => form_block.reset();
-
 const contactUsLink = document.getElementById("contactus-link");
 contactUsLink.onclick = () => form_block.classList.remove("hidden")
+
+// Make review
+let makeBtn = document.getElementById('make__review');
+let form = document.getElementById('review__form');
+
+function showForm() {
+    form.classList.remove('hidden')
+    setTimeout(() => {
+        form.classList.remove('transparent')
+    }, 200)
+}
+
+function closeForm() {
+    form.classList.add('transparent')
+    setTimeout(() => {
+        form.classList.add('hidden')
+    }, 200)
+}
